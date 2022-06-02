e61896943f107fcb5be0a4dedaa8cf3d35695223
adrianadermon <adrian.adermon@gmail.com>
2022-06-02 14:21:36 +0200

    Emacs config: added pdf path for Citar

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -283,6 +283,7 @@
   (citar-bibliography org-cite-global-bibliography)
   (bibtex-completion-bibliography org-cite-global-bibliography)
   (citar-notes-paths '("~/Dropbox/org-roam/references"))
+  (citar-library-paths '("~/Dropbox/Forskning/Zotero"))
   (citar-open-note-function 'orb-citar-edit-note))
 
 (use-package org-roam-bibtex
@@ -449,7 +450,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(org-roam-ui org-roam-bibtex citar magit corfu which-key vertico orderless embark-consult bibtex-actions consult embark marginalia deft valign auctex cdlatex org-superstar rainbow-mode olivetti org-roam evil use-package)))
+   '(org-modern org-roam-ui org-roam-bibtex citar magit corfu which-key vertico orderless embark-consult bibtex-actions consult embark marginalia deft valign auctex cdlatex org-superstar rainbow-mode olivetti org-roam evil use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
