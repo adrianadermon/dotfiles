4881e6fa12d52bcb71cf7fcee7cd38a7c85ae11a
adrianadermon <adrian.adermon@gmail.com>
2022-07-01 13:22:07 +0200

    Neovim autocompletion; Vimtex config update

--- neovim/.config/nvim/init.vim
+++ neovim/.config/nvim/init.vim
@@ -446,9 +446,8 @@ let g:vimtex_compiler_latexmk = {
 let g:vimtex_fold_enabled = 1
 
 " Use SumatraPDF as viewer with vimtex
-let g:vimtex_view_general_viewer = 'SumatraPDF'
-let g:vimtex_view_general_options
-	\ = '-reuse-instance -forward-search @tex @line @pdf'
-let g:vimtex_view_general_options_latexmk = '-reuse-instance'
+  let g:vimtex_view_general_viewer = 'SumatraPDF'
+  let g:vimtex_view_general_options
+      \ = '-reuse-instance -forward-search @tex @line @pdf'
 
 """"""""""""""""
--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -7,6 +7,42 @@ return require('packer').startup(function()
 
     use {'embark-theme/vim', as = 'embark'}
 
+    -- Snippets
+    use 'L3MON4D3/LuaSnip'
+
+    -- Completion
+    use {'hrsh7th/nvim-cmp',
+    requires = {
+        {'hrsh7th/cmp-buffer'},
+        {'hrsh7th/cmp-path'},
+        {'hrsh7th/cmp-cmdline'},
+        {'hrsh7th/cmp-omni'},
+        {'saadparwaiz1/cmp_luasnip'}},
+        config = function()
+            local cmp = require'cmp'
+            cmp.setup {
+                snippet = {
+                    expand = function(args)
+                        require('luasnip').lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-e>'] = cmp.mapping.abort(),
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                }),
+                sources = cmp.config.sources({
+                    { name = 'luasnip' },
+                    { name = 'path' },
+                    { name = 'buffer' },
+                    { name = 'omni' },
+                })
+            }
+                end
+            }
+
     -- Start screen
     use 'mhinz/vim-startify'
 
