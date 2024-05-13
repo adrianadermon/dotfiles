7d004f993155dbd8463a4834dc77ce863907222f
Adrian Adermon <adrian.adermon@gmail.com>
2024-05-13 14:02:42 +0200

    Emacs: keymaps

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -556,64 +556,49 @@
 ;;;; Consult (completion commands)
 (use-package consult
   ;; Replace bindings. Lazily loaded due by `use-package'.
-  :bind (;; C-c bindings in `mode-specific-map'
-         ("C-c M-x" . consult-mode-command)
-         ("C-c h" . consult-history)
-         ("C-c k" . consult-kmacro)
-         ("C-c m" . consult-man)
-         ("C-c i" . consult-info)
+  :bind (
          ([remap Info-search] . consult-info)
-         ;; C-x bindings in `ctl-x-map'
-         ("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
-         ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
-         ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
-         ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
-         ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
-         ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
-         ;; Custom M-# bindings for fast register access
-         ;; ("M-#" . consult-register-load)
-         ;; ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
-         ;; ("C-M-#" . consult-register)
-         ;; Other custom bindings
-         ;; ("M-y" . consult-yank-pop)                ;; orig. yank-pop
-         ;; M-g bindings in `goto-map'
-         ;; ("M-g e" . consult-compile-error)
-         ;; ("M-g f" . consult-flymake)               ;; Alternative: consult-flycheck
-         ;; ("M-g g" . consult-goto-line)             ;; orig. goto-line
-         ;; ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
-         ;; ("M-g o" . consult-outline)               ;; Alternative: consult-org-heading
-         ("M-g m" . consult-mark)
-         ("M-g k" . consult-global-mark)
-         ("M-g i" . consult-imenu)
-         ("M-g I" . consult-imenu-multi)
-         ;; M-s bindings in `search-map'
-         ("M-s d" . consult-find)
-         ("M-s f" . consult-fd)
-         ("M-s D" . consult-locate)
-         ("M-s g" . consult-grep)
-         ("M-s G" . consult-git-grep)
-         ("M-s r" . consult-ripgrep)
-         ("M-s l" . consult-line)
-         ("M-s L" . consult-line-multi)
-         ("M-s k" . consult-keep-lines)
-         ("M-s u" . consult-focus-lines)
-         ;; Isearch integration
-         ("M-s e" . consult-isearch-history)
-         :map isearch-mode-map
-         ("M-e" . consult-isearch-history)         ;; orig. isearch-edit-string
-         ("M-s e" . consult-isearch-history)       ;; orig. isearch-edit-string
-         ("M-s l" . consult-line)                  ;; needed by consult-line to detect isearch
-         ("M-s L" . consult-line-multi)            ;; needed by consult-line to detect isearch
-         ;; Minibuffer history
-         :map minibuffer-local-map
-         ;; ("M-s" . consult-history)                 ;; orig. next-matching-history-element
-         ;; ("M-r" . consult-history)                 ;; orig. previous-matching-history-element
          )
-
-  ;; The :init configuration is always executed (Not lazy)
+  :general
+  (:prefix "C-c"
+           "M-x" 'consult-mode-command
+           "h" 'consult-history
+           "k" 'consult-kmacro
+           "m" 'consult-man
+           "i" 'consult-info)
+  (:prefix "C-x"
+           "M-:" 'consult-complex-command     ;; orig. repeat-complex-command
+           "b" 'consult-buffer                ;; orig. switch-to-buffer
+           "4 b" 'consult-buffer-other-window ;; orig. switch-to-buffer-other-window
+           "5 b" 'consult-buffer-other-frame  ;; orig. switch-to-buffer-other-frame
+           "r b" 'consult-bookmark            ;; orig. bookmark-jump
+           "p b" 'consult-project-buffer)      ;; orig. project-switch-to-buffer
+  (:prefix "M-g"
+           "m" 'consult-mark
+           "k" 'consult-global-mark
+           "i" 'consult-imenu
+           "I" 'consult-imenu-multi)
+  (:prefix "M-s"
+           "d" 'consult-find
+           "f" 'consult-fd
+           "D" 'consult-locate
+           "g" 'consult-grep
+           "G" 'consult-git-grep
+           "r" 'consult-ripgrep
+           "l" 'consult-line
+           "L" 'consult-line-multi
+           "k" 'consult-keep-lines
+           "u" 'consult-focus-lines
+           "e" 'consult-isearch-history)
+  (:keymaps 'isearch-mode-map
+            "M-e" 'consult-isearch-history         ;; orig. isearch-edit-string
+            :prefix "M-s"
+            "e" 'consult-isearch-history       ;; orig. isearch-edit-string
+            "l" 'consult-line                  ;; needed by consult-line to detect isearch
+            "L" 'consult-line-multi)            ;; needed by consult-line to detect isearch
+    ;; The :init configuration is always executed (Not lazy)
   :init
-  
-  ;; Use Consult to select xref locations with preview
+    ;; Use Consult to select xref locations with preview
   (setq xref-show-xrefs-function #'consult-xref
         xref-show-definitions-function #'consult-xref)
 )
@@ -1014,14 +999,15 @@
     "Show structure of R object at point"
     (interactive)
     (apply-r-func-at-point "str"))
-  :bind (:map ess-r-mode-map
-              ("C-c <" . ess-insert-assign)
-              ("C-c >" . r-insert-magrittr-pipe)
-              ("C-c r s" . r-summary-at-point)
-              ("C-c r p" . r-print-at-point)
-              ("C-c r n" . r-names-at-point)
-              ("C-c r t" . r-structure-at-point)
-              )
+  :general
+  (:keymap 'ess-r-mode-map
+           :prefix "C-c"
+           "<" 'ess-insert-assign
+           ">" 'r-insert-magrittr-pipe
+           "r s" 'r-summary-at-point
+           "r p" 'r-print-at-point
+           "r n" 'r-names-at-point
+           "r t" 'r-structure-at-point)
   :custom
   (inferior-R-args "--no-save --no-restore")
   (ess-indent-offset 2)
