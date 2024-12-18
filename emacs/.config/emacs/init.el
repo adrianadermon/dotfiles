4a85daef24abdc0a8cbbf461bf4d7353bd2f62e5
Adrian Adermon <adrian.adermon@gmail.com>
2024-12-18 15:38:56 +0100

    Emacs: fix

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1409,7 +1409,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
- ;; Local Variables:
+;; Local Variables:
 ;; outline-regexp: ";;; \\|;;;; "
 ;; eval: (outline-minor-mode)
 ;; End:
