2d5a45404a7925187118b2651dd80ad4c3f85f53
adrianadermon <adrian.adermon@gmail.com>
2025-02-07 13:39:56 +0100

    Emacs

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1461,7 +1461,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-vc-selected-packages
-   '((typst-ts-mode :url "https://codeberg.org/meow_king/typst-ts-mode"))))
+   '((ultra-scroll :url "https://github.com/jdtsmith/ultra-scroll"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
