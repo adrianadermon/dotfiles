d5b39ea845d47b0766ffec3ba62dbd0b00dcf3c2
adrianadermon <adrian.adermon@gmail.com>
2025-03-20 13:21:42 +0100

    Emacs

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1555,7 +1555,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-vc-selected-packages '((aider :url "https://github.com/tninja/aider.el"))))
+ '(package-vc-selected-packages
+   '((typst-ts-mode :url "https://codeberg.org/meow_king/typst-ts-mode"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
